2e1e387973f6c419d70f2bb773fc33076d7cb166
Mian Mannan <mianmannan98@gmail.com>
2022-12-03 01:23:41 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import HomePage from './HomePage.js';
+import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
+
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (forName) => {
+    setCurrentForm = (forName);
+  }
+
+  return (
+    <div className="App">
+      {
+        currentForm === "login"? <Login onFormSwitch = {toggleForm} /> : <Register onFormSwitch = {toggleForm} />
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,26 @@
+import './HomePage.css'
+
+
+
+function HomePage()
+{
+    return(
+            <div className = "HomePage">
+                <nav className = "navbar">
+                    <ul>
+                        <li>Travelry</li>
+                        <li>Travel</li>
+                        <li>Hotels</li>
+                        <li>Places</li>
+                        <li>Weather</li>
+                        <li>To Do's</li>
+                        <li>Uploads</li>
+                        <li>Sign In</li>
+                        <li>Register</li>
+                    </ul>
+                </nav>
+            </div>
+    );
+}
+
+export default HomePage;
